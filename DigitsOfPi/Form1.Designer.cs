31d8e7f7a93db7e4ec853dd1dd78adbff3f0c375
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-12-30 19:19:40 +0200

    Created project DigitsOfPi

---
+++ DigitsOfPi/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DigitsOfPi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberOfDigitsLabel = new System.Windows.Forms.Label();
+            this.numbericUpDnDigitsOfPi = new System.Windows.Forms.NumericUpDown();
+            this.calculatePiBtn = new System.Windows.Forms.Button();
+            this.piTextBox = new System.Windows.Forms.TextBox();
+            this.piProgressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numbericUpDnDigitsOfPi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numberOfDigitsLabel
+            // 
+            this.numberOfDigitsLabel.AutoSize = true;
+            this.numberOfDigitsLabel.Location = new System.Drawing.Point(13, 13);
+            this.numberOfDigitsLabel.Name = "numberOfDigitsLabel";
+            this.numberOfDigitsLabel.Size = new System.Drawing.Size(57, 13);
+            this.numberOfDigitsLabel.TabIndex = 0;
+            this.numberOfDigitsLabel.Text = "Digits of Pi";
+            // 
+            // numbericUpDnDigitsOfPi
+            // 
+            this.numbericUpDnDigitsOfPi.Location = new System.Drawing.Point(76, 11);
+            this.numbericUpDnDigitsOfPi.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numbericUpDnDigitsOfPi.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numbericUpDnDigitsOfPi.Name = "numbericUpDnDigitsOfPi";
+            this.numbericUpDnDigitsOfPi.Size = new System.Drawing.Size(56, 20);
+            this.numbericUpDnDigitsOfPi.TabIndex = 1;
+            this.numbericUpDnDigitsOfPi.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // calculatePiBtn
+            // 
+            this.calculatePiBtn.Location = new System.Drawing.Point(138, 8);
+            this.calculatePiBtn.Name = "calculatePiBtn";
+            this.calculatePiBtn.Size = new System.Drawing.Size(75, 23);
+            this.calculatePiBtn.TabIndex = 2;
+            this.calculatePiBtn.Text = "Calc";
+            this.calculatePiBtn.UseVisualStyleBackColor = true;
+            this.calculatePiBtn.Click += new System.EventHandler(this.calculatePiBtn_Click);
+            // 
+            // piTextBox
+            // 
+            this.piTextBox.Location = new System.Drawing.Point(12, 37);
+            this.piTextBox.Multiline = true;
+            this.piTextBox.Name = "piTextBox";
+            this.piTextBox.ReadOnly = true;
+            this.piTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.piTextBox.Size = new System.Drawing.Size(206, 165);
+            this.piTextBox.TabIndex = 3;
+            // 
+            // piProgressBar
+            // 
+            this.piProgressBar.Location = new System.Drawing.Point(12, 210);
+            this.piProgressBar.Name = "piProgressBar";
+            this.piProgressBar.Size = new System.Drawing.Size(201, 23);
+            this.piProgressBar.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 245);
+            this.Controls.Add(this.piProgressBar);
+            this.Controls.Add(this.piTextBox);
+            this.Controls.Add(this.calculatePiBtn);
+            this.Controls.Add(this.numbericUpDnDigitsOfPi);
+            this.Controls.Add(this.numberOfDigitsLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numbericUpDnDigitsOfPi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberOfDigitsLabel;
+        private System.Windows.Forms.NumericUpDown numbericUpDnDigitsOfPi;
+        private System.Windows.Forms.Button calculatePiBtn;
+        private System.Windows.Forms.TextBox piTextBox;
+        private System.Windows.Forms.ProgressBar piProgressBar;
+    }
+}
+
---
+++ DigitsOfPi/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitsOfPi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ShowProgress(string pi, int totalDigits, int digitsSoFar)
+        {
+            piProgressBar.Text = pi;
+            piProgressBar.Maximum = totalDigits;
+            piProgressBar.Value = digitsSoFar;
+        }
+
+        async Task CalcPi(int digits)
+        {   
+            StringBuilder pi = new StringBuilder("3", digits + 2);
+            // Show progress
+            ShowProgress(pi.ToString(), digits, 0);
+            if (digits > 0)
+            {
+                pi.Append(".");
+                for (int i = 0; i < digits; i += 9)
+                {
+                    int nineDigits = await Task.Run(() => NineDigitsOfPi.StartingAt(i + 1));
+                    int digitCount = Math.Min(digits - i, 9);
+                    string ds = string.Format("{0:D9}", nineDigits);
+                    pi.Append(ds.Substring(0, digitCount));
+                    // Show progress
+                    ShowProgress(pi.ToString(), digits, i + digitCount);
+                    piTextBox.Text = pi.ToString();
+
+                    // autoscroll
+                    piTextBox.Select(i, 1);
+                    piTextBox.ScrollToCaret();
+                }
+            }
+        }
+
+        private async void calculatePiBtn_Click(object sender, EventArgs e)
+        {
+            await CalcPi(Convert.ToInt32(numbericUpDnDigitsOfPi.Value));
+            MessageBox.Show("Done");
+        }
+    }
+}
